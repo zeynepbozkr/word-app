0910bfcb9af71a13d644df958c0bcc7b773f20ab
zeynepbozkr <73405507+zeynepbozkr@users.noreply.github.com>
2021-10-01 16:12:47 +0300

    form fixed

--- components/addPost.js
+++ components/addPost.js
@@ -1,15 +1,81 @@
+// import React, { useState } from "react";
+// import "antd/dist/antd.css";
+// import { Formik } from "formik";
+// import { Form, Input, Button, Select, Divider } from "antd";
+
+// function addPost() {
+//   const [add, setAdd] = useState([{ email: "uy", message: "ty", url: "y" }]);
+//   const onFinish = (values) => {
+//     console.log("Success:", values);
+//   };
+//   return (
+//     <>
+//       <Formik
+//         initialValues={{ email: "", url: "", message: "" }}
+//         onSubmit={(values) => {
+//           console.log("df", values);
+//         }}
+//       >
+//         {({
+//           values,
+//           errors,
+//           touched,
+//           handleChange,
+//           handleBlur,
+//           handleSubmit,
+//           isSubmitting,
+//         }) => (
+//           <Form onFinish={onFinish}>
+//             <Form.Item
+//               value="url"
+//               name="url"
+//               label="URL"
+//               rules={[
+//                 { required: true, message: "Please input your username!" },
+//               ]}
+//             >
+//               <Input value="url" name="url" />
+//             </Form.Item>
+//             <Form.Item
+//               name={["user", "email"]}
+//               label="Email"
+//               rules={[
+//                 {
+//                   type: "email",
+//                   required: true,
+//                   message: "Please input your username!",
+//                 },
+//               ]}
+//             >
+//               <Input value="email" />
+//             </Form.Item>
+//             <Form.Item label="Message" name="message">
+//               <Input.TextArea value="message" />
+//             </Form.Item>
+//             <Form.Item label="Button">
+//               <Button onClick={handleSubmit}>Button</Button>
+//             </Form.Item>
+//           </Form>
+//         )}
+//       </Formik>
+//     </>
+//   );
+// }
+
+//export default addPost;
+
 import React, { useState } from "react";
 import "antd/dist/antd.css";
 
 import { PlusOutlined } from "@ant-design/icons";
-import { Form, Input, Button, Select, Divider } from "antd";
+import { Form, FormList, Input, Button, Select, Divider } from "antd";
 import ListPage from "../pages/listPage";
 
 const addPost = () => {
   const { Option } = Select;
   const { TextArea } = Input;
 
-  const [add, setAdd] = useState([]);
+  const [add, setAdd] = useState([{ url: "", email: "", message: "" }]);
 
   const [addItem, setAddItem] = useState(["word1", "word2", "word3"]);
 
@@ -23,10 +89,17 @@ const addPost = () => {
   //     console.log(":", word);
   //   };
 
+  const onList = () => {
+    console.log(add, "NEWSDA");
+  };
+
   const onFinish = (e) => {
-    console.log(e);
-    setAdd({ url: e.url, email: e.email, message: e.message });
-    console.log(add.message, "add");
+    let arr = { url: e.url, email: e.email, message: e.message };
+    let newArr = [...add];
+    newArr.push(arr);
+
+    setAdd(newArr);
+    console.log(add, "bekirciğimmmsödjfjfdkjzslkjfgdkjhxfökgx");
   };
 
   const handlesubmit = () => {};
@@ -54,33 +127,28 @@ const addPost = () => {
         >
           <Input />
         </Form.Item>
-        <Form.Item
-          label="Select"
-          rules={[
-            {
-              required: true,
-              message: "Please input your username!",
-            },
-          ]}
-        >
-          <Select node="multiple" style={{ width: 240 }} placeholder="color">
-            {addItem.map((item, index) => {
-              return (
-                <Select.Option key={index} value={item}>
-                  {item}
-                </Select.Option>
-              );
-            })}
-          </Select>
-          <br /> <br />
-          <Form.Item label="Message" name="message">
-            <Input.TextArea />
-          </Form.Item>
+
+        <Form.Item label="Message" name="message">
+          <Input.TextArea />
         </Form.Item>
         <Form.Item label="Button">
-          <Button htmlType="submit">Button</Button>
+          <Button htmlType="submit">+</Button>
+        </Form.Item>
+        <Form.Item label="Button">
+          <Button onClick={onList}>Button</Button>
         </Form.Item>
       </Form>
+      <Form.List>
+        {(addItem) => (
+          <div>
+            {addItem.map((field) => (
+              <Form.Item {...field}>
+                <Input />
+              </Form.Item>
+            ))}
+          </div>
+        )}
+      </Form.List>
 
       {/* <Button href="/listPage" onClick={handlesubmit}>
         GO
